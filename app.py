f230a52f928c670f632e4e7f5d3d1c1d3e77554a
TaraRhoseyn <taragulwell@gmail.com>
2021-10-19 18:54:11 +0000

    Add functionality to login form

--- app.py
+++ app.py
@@ -40,11 +40,35 @@ def register():
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
         }
+        
         mongo.db.users.insert_one(register)
 
         session["user"] = request.form.get("username").lower()
         flash("You are now registered!")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+    
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome back! You're logged in")
+            else:
+                flash("Whoops! Incorrect username or password.")
+                return redirect(url_for("login"))
+
+        else:
+            flash("Whoops! Incorrect username or password.")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+  
 
 
 if __name__ == "__main__":
