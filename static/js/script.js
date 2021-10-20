403e1197924c081f82950a4411fe73a052e1ff26
TaraRhoseyn <taragulwell@gmail.com>
2021-10-20 18:51:48 +0000

    docs: Adjust comments and spacing between code for readability

--- static/js/script.js
+++ static/js/script.js
@@ -5,11 +5,11 @@ $(document).ready(function(){
     $(".button-collapse").sideNav();
   });
 
-$("#read_status").on("click", showFormContent);
 
 // Remove or show more form content depending on if user has read book
 // Credit: Stack Overflow
 // Bug fix: use classList.toggle instead of if/else logic
+$("#read_status").on("click", showFormContent);
 function showFormContent(){
   document.getElementById('extra-content').classList.toggle("hidden");
 }
