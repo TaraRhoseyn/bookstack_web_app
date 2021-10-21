0a7af047e48fc4a1e86afa06b41017a6470b98ef
TaraRhoseyn <taragulwell@gmail.com>
2021-10-21 19:28:34 +0000

    Add input fields for book review and date read if user selects book as read

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,20 @@
 /* jshint esversion: 6 */
 
-$(document).ready(function(){
-    // Init Materialize components
-    $(".sidenav").sidenav();
-    $(".button-collapse").sideNav();
+$(document).ready(function () {
+  // Init Materialize components
+  $(".sidenav").sidenav;
+  $(".button-collapse").sidenav();
+  $(".collapsible").collapsible();
+  $(".tooltipped").tooltip();
+  $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 3,
+      showClearBtn: true,
+      i18n: {
+          done: "Select"
+      }
   });
+});
 
 // Remove or show more form content depending on if user has read book
 // Credit: Stack Overflow
