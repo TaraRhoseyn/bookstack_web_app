69e9786a5c49ae16ff7331e429fd742b67ae8f1e
TaraRhoseyn <taragulwell@gmail.com>
2021-10-23 13:58:07 +0000

    Add blueprint model of codebase structure - files only

--- run.py
+++ run.py
@@ -81,7 +81,7 @@ def add_book():
             "is_read": is_read,
             "added_by": session["user"],
             "book_review": request.form.get("book_review"),
-            "date_read": request.form.get("read_date"),
+            "date_read": request.form.get("date_read"),
         }
     
         if is_read == "yes":
