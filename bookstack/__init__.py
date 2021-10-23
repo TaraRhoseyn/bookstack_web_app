5dfe454683eb0f90de1e6ab79c0f295a3a709c21
TaraRhoseyn <taragulwell@gmail.com>
2021-10-23 20:15:42 +0000

    Update app.py and __init__.py to be able to run app from blueprints

--- bookstack/__init__.py
+++ bookstack/__init__.py
@@ -1,15 +1,11 @@
 import os
-from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for)
+from flask import (Flask)
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash
+
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
@@ -17,7 +13,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 mongo.init_app(app)
 
-def init_app():
+def create_app():
     """
     Initialize the app with blueprint routes
     """
@@ -32,3 +28,4 @@ def init_app():
     app.register_blueprint(books)
     app.register_blueprint(main)
     # Return app
+    return app
