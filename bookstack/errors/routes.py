6f3f7c99c2e5964a491de785a2d88e75eba090f0
TaraRhoseyn <taragulwell@gmail.com>
2021-10-30 13:25:02 +0000

    Update 404 error handling function to be routed through the main app

--- bookstack/errors/routes.py
+++ bookstack/errors/routes.py
@@ -1,14 +1,16 @@
 from flask import (
     Blueprint, Flask, render_template,)
 
+
+from bookstack import app
 errors = Blueprint('errors', __name__)
 
 
-@errors.errorhandler(404)
-def not_found(e):
+@app.errorhandler(404)
+def error_404(error: object) -> object:
     """
     This function renders the 404.html template
     if a 404 error is triggered.
     :return render_template of 404.html
     """
-    return render_template("404.html")
+    return render_template('404.html', error=error), 404
