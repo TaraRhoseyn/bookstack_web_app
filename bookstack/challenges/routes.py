8655c8c779a6ac593097e0ac5110b70b6d4c3f79
TaraRhoseyn <taragulwell@gmail.com>
2021-10-30 07:51:40 +0000

    Add functions to challenges/routes.py that give users CRUD functionality in relation to challenges

--- bookstack/challenges/routes.py
+++ bookstack/challenges/routes.py
@@ -5,4 +5,59 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from bookstack import mongo
 
-reviews = Blueprint('challenges', __name__)
+challenges = Blueprint('challenges', __name__)
+
+@challenges.route("/add_challenge", methods=["GET", "POST"])
+def add_challenge() -> object:
+    """
+    This function adds challenges to the 'challenges'
+    collection.
+    :return render_template of add_challenge.html
+    """
+    if request.method == "POST":
+        challenge = {
+            "challenge_name": request.form.get("challenge_name"),
+            "added_by": session["user"]
+        }
+        mongo.db.challenges.insert_one(challenge)
+        flash("Challenge added!")
+        return redirect(url_for("main.dashboard", username=session["user"]))
+    return render_template("add_challenge.html")
+
+
+@challenges.route("/edit_challenge/<challenge_id>", methods=["GET", "POST"])
+def edit_challenge(challenge_id) -> object:
+    """
+    This function uses bson ObjectId
+    to pull data from specific document within
+    the 'challenges' collection, then pushes editted
+    data back to the collection in the db to be
+    updated.
+    :return render_template of edit_challenge.html
+    """
+    if request.method == "POST":
+        is_complete = "yes" if request.form.get("complete_status") else "no"
+        editted_challenge = {
+            "challenge_name": request.form.get("challenge_name"),
+            "added_by": session["user"],
+            "is_complete": is_complete
+        }
+        mongo.db.challenges.update(
+            {"_id": ObjectId(challenge_id)}, editted_challenge)
+        flash("challenge editted!")
+
+    challenge = mongo.db.challenges.find_one({"_id": ObjectId(challenge_id)})
+    return render_template("edit_challenge.html", challenge=challenge)
+
+
+@challenges.route("/delete_challenge/<challenge_id>")
+def delete_challenge(challenge_id) -> object:
+    """
+    This function deletes challenge documents
+    from the challenges collection.
+    :return url_for of main.dashboard
+    """
+    mongo.db.challenges.remove({"_id": ObjectId(challenge_id)})
+    flash(
+        "challenge removed. To remove the book from your stacks, please visit the Stacks page.")
+    return redirect(url_for("main.dashboard"))
