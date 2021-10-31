763a5c71425fd79f2b3e65f3da75fca9273b2a51
TaraRhoseyn <taragulwell@gmail.com>
2021-10-31 14:44:43 +0000

    fix: Add black-text utility class for accessibility

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = create_app()
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
